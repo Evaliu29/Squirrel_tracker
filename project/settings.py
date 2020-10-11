d085d58d8abc2a5cbd52694f41644a9f137b7672
Minyue <ml4368@columbia.edu>
2020-10-11 19:07:04 +0000

    Add squirrel application and models

--- project/settings.py
+++ project/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'squirrel'
 ]
 
 MIDDLEWARE = [
@@ -73,13 +74,18 @@ WSGI_APPLICATION = 'project.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
+#DATABASES = {
+ #   'default': {
+  #      'ENGINE': 'django.db.backends.sqlite3',
+   #     'NAME': BASE_DIR / 'db.sqlite3',
+    #}
+#}
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+    "default": {
+        "ENGINE": "django.db.backends.sqlite3",
+        "NAME": str(BASE_DIR / "db.sqlite3"),
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/3.1/ref/settings/#auth-password-validators
---
+++ squirrel/models.py
@@ -0,0 +1,110 @@
+from django.db import models
+from django.utils.translation import gettext as _
+# Create your models here.
+class Squirrel(models.Model):
+    Latitude = models.FloatField(
+            help_text=_('Latitude')
+            )
+
+    Longitude = models.FloatField(
+            help_text=_('Longitude')
+            )
+
+    Unique_Squirrel_ID = models.CharField(
+            max_length=255,
+            help_text=_('Unique Squirrel ID'),
+            primary_key=True
+            )
+
+    Shift = models.CharField(
+            max_length=100,
+            help_text=_('Shift')
+            )
+
+    Date = models.DateField(
+            help_text=_('Date')
+            )
+
+    Age = models.CharField(
+             max_length = 255,
+             help_text=_('Age')
+             )
+
+    
+    Primary_Fur_Color = models.CharField(
+             max_length = 255,
+             help_text=_('Primary Fur Color')
+             )
+
+    Location = models.CharField(
+             max_length = 255,
+             help_text=_('Location')
+             )
+
+    Specific_Location = models.CharField(
+             max_length = 255,
+             help_text=_('Specific Location')
+             )
+     
+    Running = models.BooleanField(
+            help_text=_("Running")
+            )
+
+    Chasing = models.BooleanField(
+            help_text=_("Chasing")
+            )
+
+    Climbing = models.BooleanField(
+            help_text=_("Climbing")
+            )
+
+    Eating = models.BooleanField(
+            help_text=_("Eating")
+            )
+
+    Foraging = models.BooleanField(
+            help_text=_("Foraging")
+            )
+
+    Other_Activities = models.CharField(
+            max_length=255,
+            help_text=_("Other Activities")
+            )
+
+    Kuks = models.BooleanField(
+            help_text=_("Kuks")
+            )
+
+    Quaas = models.BooleanField(
+            help_text=_("Quaas")
+            )
+
+    Moans = models.BooleanField(
+            help_text=_("Moans")
+            )
+
+    Tail_flags = models.BooleanField(
+            help_text=_("Tail flags")
+            )
+    
+    Tail_twitches = models.BooleanField(
+            help_text=_("Tail twitches")
+            )
+
+    Approaches = models.BooleanField(
+            help_text=_("Approaches")
+            )
+    
+    Indifferent = models.BooleanField(
+            help_text=_("Indifferent")
+            )
+
+
+    Runs_from = models.BooleanField(
+            help_text=_("Runs from")
+            )
+
+
+     
+
+
