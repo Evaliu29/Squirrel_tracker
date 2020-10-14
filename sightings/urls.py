ea74ec2189005b47062e7d8b60a7fa4ddc7d386e
Sylvie Qiang <xq2212@columbia.edu>
2020-10-14 01:15:33 +0000

    Create stats function in sightings/views.

--- sightings/urls.py
+++ sightings/urls.py
@@ -6,6 +6,7 @@ app_name = 'sightings'
 urlpatterns = [
         path('', views.lists, name='lists'),
         path('create/',views.create,name='create'),
+        path('stats/', views.stats, name='stats'),
         path('<str:squirrel_id>/',views.update,name='update')
 
         ]
--- sightings/views.py
+++ sightings/views.py
@@ -5,6 +5,8 @@ from .forms import SquirrelForm, SquirrelFullForm
 from django.http import JsonResponse
 from django.http import HttpResponse
 from django.shortcuts import redirect
+from django.db.models import Avg, Max, Min, Count
+
 
 def lists(request):
     sightings = Squirrel.objects.all()
@@ -45,5 +47,27 @@ def create(request):
             'fullform':fullform
             }
     return render(request, 'sightings/create.html', context)
+
+def stats(request):
+    count = Squirrel.object.count()
+    ratio = Squirrel.objects.filter(Shift = 'AM' ).count()/Squirrel.objects.filter(Shift = 'PM' ).count()
+    avg_lat  = Squirrel.objects.aggregate(Avg('Latitude'))
+    avg_long = Squirrel.objects.aggregate(Avg('Longitude'))
+    adult_no = Squirrel.objects.filter(Age = 'Adult' ).count()
+    run_perct = Squirrel.objects.filter(Running = True ).count()/count
+
+
+    context = {
+            'Count_Squirrels': count,
+            'Avg_Lattitude': avg_lat,
+            'Avg_Longitude': avg_long
+            'AM/PM ratio': ratio
+            'Adult_Squirrel_Count': adult_no
+            'Running_perct': run_perct
+            }
+
+    return render(request, 'sightings/stats.html', context)
+
+
 
 # Create your views here.
